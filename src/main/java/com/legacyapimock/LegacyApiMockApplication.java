a33aa8b681e7c1713cd140cfebc7390f596fe13a
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-30 08:15:59 +0300

    chore: initialize project.

---
+++ src/main/java/com/legacyapimock/LegacyApiMockApplication.java
@@ -0,0 +1,12 @@
+package com.legacyapimock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyApiMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegacyApiMockApplication.class, args);
+    }
+}
